0cbf94f5d457987d999f78977e4e13afdb9b9716
Tamerlan Kengerli <tamerlankengerli2002@gmail.com>
2024-03-14 03:36:07 +0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import Features from "./components/Features"
+import Footer from "./components/Footer"
+import Hero from "./components/Hero"
+import Navbar from "./components/Navbar"
+import Pricing from "./components/Pricing"
+import Testimonials from "./components/Testimonials"
+import Workflow from "./components/Workflow"
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <main className="max-w-7xl mx-auto pt-20 px-6">
+        <Hero />
+        <Features />
+        <Workflow />
+        <Pricing />
+        <Testimonials />
+        <Footer />
+      </main>
+    </>
+  )
+}
+
+export default App
